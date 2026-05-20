d43ecfc87cb8d4ab18878eaf3dc46242717c1070
nayakbiranchi600-lang <nayakbiranchi600@gmail.com>
2026-05-20 08:23:49 +0000

    array fist class

---
+++ .devcontainer/02_basic/01_array.js
@@ -0,0 +1,54 @@
+// Array
+
+const myArray =  [1,2,3,4,5]
+const myHeroes = ["Saktiman","Nagraj","Kriss"]
+const myArray2 = new Array(1,2,3,4,5)
+
+// console.log(myArray);
+// console.log(myArray[1]);
+
+// console.log(myHeroes);
+// console.log(myHeroes[1]);
+
+// console.log(myArray2);
+// console.log(myArray2[1]);
+
+// Array Methods
+
+// myArray.push(6)
+// console.log(myArray);
+// myArray.pop()
+// console.log(myArray);
+
+// myArray.unshift(6)
+// console.log(myArray);
+// myArray.shift()
+// console.log(myArray);
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(15));
+// console.log(myArray.indexOf(3));
+
+// const newArray = myArray.join() // Converted array into string
+// console.log(newArray);
+// console.log(typeof newArray);
+
+// Slice and Splice
+console.log(myArray);
+console.log(myArray.slice(1,3));
+console.log(myArray);
+
+console.log(myArray.splice(1,3));
+console.log(myArray);
+
+
+
+
+
+
+
+
+
+
+
+ 
