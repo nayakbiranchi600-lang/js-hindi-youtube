a4744eeaa92fc26f8264a522541f0003f0d4d3a2
nayakbiranchi600-lang <nayakbiranchi600@gmail.com>
2026-05-21 07:50:06 +0000

    object first class

---
+++ .devcontainer/02_basic/03_object.js
@@ -0,0 +1,32 @@
+// singleton
+//Object.create (contructor method)//singleton create here
+
+//Object literals
+const mySym = Symbol("key1")
+const jsUser = {
+    name : "Biranchi",
+    "fullName":"Biranchi Narayan Nayak",
+    age : 24,
+    location : "Bhadrak",
+    [mySym] : "myKey1",
+    email: "nayak@google.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday","Saturday"]
+}
+console.log(jsUser.email);
+console.log(jsUser['email']);
+console.log(jsUser['name']);
+console.log(jsUser.fullName);
+console.log(jsUser['fullName']);
+console.log(jsUser[mySym]);
+console.log(typeof jsUser[mySym]);
+
+jsUser.email = "nayakbiranchi@gail.com"
+console.log(jsUser);
+Object.freeze(jsUser);//after freeze object any value cannot be change
+jsUser.email = "nayak123@gmail.com"
+console.log(jsUser);
+
+
+
+
