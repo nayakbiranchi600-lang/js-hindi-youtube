4c4b2b7438fcdcb4971617710037476a4a139826
nayakbiranchi600-lang <nayakbiranchi600@gmail.com>
2026-05-27 11:38:12 +0000

    control flow under processs

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,60 @@
+// If
+
+// if (3!=2) {
+//     console.log("Executed");  
+// }
+
+//<,>,<=,>=,==,===,!==
+
+// if (2==="2") { // its return true
+//     console.log(true);  
+// }
+
+// if (2==="2") { // its return false because both are not same datatype (=== checks datatype)
+//     console.log(true);  
+// }
+
+// const temperature = 41
+// if(temperature === 40){
+//     console.log("Temperature under 50 degree celcious");  
+// }else{
+//     console.log("Temperature is greaterthan 50 degree celcious");
+// }
+
+// const score = 200
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User power:${power}`);   
+// }
+//  console.log(`User power:${power}`); 
+
+//const balance = 1000;
+//// if(balance>500)console.log("Test"); // implicit scope (its execute in one line)
+
+// if(balance<500){
+//     console.log("less than 500");   
+// }else if(balance<700){
+//     console.log("less then 700");
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less then 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow buy course");
+}
+if(userLoggedIn && debitCard && 2==3){
+    console.log("Allow buy 2course");
+}
+
+const loggedInFromGoggle = true
+const loggedInFromEmail = true
+const guastLoggedIn = true
+
+if(loggedInFromGoggle && loggedInFromEmail && guastLoggedIn){
+    console.log("User Logged in");   
+}
