c3791b1a077f341a8ecdaaab3622f7657aa73f38
nayakbiranchi600-lang <nayakbiranchi600@gmail.com>
2026-06-15 07:52:02 +0000

    forEach,filter,map,reduce

--- .devcontainer/05_iterations/eight.js
+++ .devcontainer/05_iterations/eight.js
@@ -1,7 +1,31 @@
 const myNums = [1,2,3,4,5]
-// const myTotal = myNums.reduce(function(acc,currentVal){
-//     console.log(`acc : ${acc} and curerentVal : ${currentVal}`);
-//     return acc + currentVal
-// },0)
-const myTotal = myNums.reduce((acc,currentVal)=>acc+currentVal,0)
-console.log(myTotal);
+const myTotal = myNums.reduce(function(acc,curntval){
+    return acc+curntval
+},0)
+console.log((myTotal));
+
+const myimplisit_return = myNums.reduce((acc,curr)=>(acc+curr),0)
+console.log(myimplisit_return)
+
+const shoppingCart = [
+    {
+        itemName:"Js Course",
+        price: 999
+    },
+    {
+        itemName:"Java Course",
+        price: 599
+    },
+    {
+        itemName:"Python Course",
+        price: 399
+    },
+    {
+        itemName:"PHP Course",
+        price: 1299
+    }
+]
+
+const totalPrice = shoppingCart.reduce((acc,item)=> (acc+item.price),0)
+console.log(totalPrice);
+
